2639a2c612ddbfc37841c9bae2a1d522128a5a39
Samuel1505 <oluwakoredeakinola@gmail.com>
2025-11-18 22:50:37 +0100

    removed modifiers

--- smartcontract/contracts/vote.sol
+++ smartcontract/contracts/vote.sol
@@ -40,4 +40,206 @@ contract VotingContract is AccessControl {
         bool exists;
     }
 
+    address[] public contendersList;
+    mapping(address => bool) public voted;
+    mapping(string => address) public codeToAddress;
+    mapping(address => address) public voterToContender;
+    mapping(address => ContDetails) public contenderDetails;
+
+   
+
+    constructor() {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(REGISTRAR_ROLE, msg.sender);
+    }
+
+    // Register a single contender
+    function registerContender(address cont, string memory code) public onlyRegistrar votingNotStarted {
+        require(cont != address(0), "Invalid contender address");
+        require(bytes(code).length > 0, "Code cannot be empty");
+        require(codeToAddress[code] == address(0), "Code already exists");
+        require(!contenderDetails[cont].exists, "Contender already registered");
+
+        codeToAddress[code] = cont;
+        contendersList.push(cont);
+        
+        ContDetails storage c = contenderDetails[cont];
+        c.contender = cont;
+        c.code = code;
+        c.votersNo = 0;
+        c.exists = true;
+
+        emit ContenderRegistered(cont, code);
+    }
+
+    // Register multiple contenders at once
+    function registerMultipleContenders(
+        address[] memory contenders, 
+        string[] memory codes
+    ) public onlyRegistrar votingNotStarted {
+        require(contenders.length == codes.length, "Arrays length mismatch");
+        require(contenders.length > 0, "No contenders provided");
+
+        for (uint256 i = 0; i < contenders.length; i++) {
+            registerContender(contenders[i], codes[i]);
+        }
+    }
+
+    // Start the voting process
+    function startVoting() public onlyRegistrar votingNotStarted {
+        require(contendersList.length > 0, "No contenders registered");
+        
+        votingActive = true;
+        votingStartTime = block.timestamp;
+        votingEndTime = block.timestamp + VOTING_DURATION;
+    }
+
+    // Cast a vote
+    function vote(string memory code) public votingIsActive {
+        address sender = msg.sender;
+        require(!voted[sender], "User already voted");
+        
+        address contender = codeToAddress[code];
+        require(contender != address(0), "Invalid contender code");
+        require(contenderDetails[contender].exists, "Contender does not exist");
+
+        voted[sender] = true;
+        voterToContender[sender] = contender;
+        
+        ContDetails storage m = contenderDetails[contender];
+        m.votersNo += 1;
+        
+        emit VoteSuccess(sender, contender, code);
+    }
+
+    // End voting and determine winners
+    function endVoting() public onlyRegistrar {
+        require(votingActive, "Voting is not active");
+        require(block.timestamp > votingEndTime || votingEnded, "Voting period not ended");
+        
+        votingActive = false;
+        votingEnded = true;
+
+        // Find highest vote count
+        uint32 highestVotes = 0;
+        for (uint256 i = 0; i < contendersList.length; i++) {
+            uint32 votes = contenderDetails[contendersList[i]].votersNo;
+            if (votes > highestVotes) {
+                highestVotes = votes;
+            }
+        }
+
+        // Collect all winners (in case of tie)
+        address[] memory winners = new address[](contendersList.length);
+        uint256 winnerCount = 0;
+        
+        for (uint256 i = 0; i < contendersList.length; i++) {
+            if (contenderDetails[contendersList[i]].votersNo == highestVotes) {
+                winners[winnerCount] = contendersList[i];
+                winnerCount++;
+            }
+        }
+
+        // Resize winners array
+        address[] memory finalWinners = new address[](winnerCount);
+        for (uint256 i = 0; i < winnerCount; i++) {
+            finalWinners[i] = winners[i];
+        }
+
+        emit VotingEnded(finalWinners, highestVotes);
+    }
+
+    // Transfer the registrar role to a new address
+    function transferRegistrarRole(address newRegistrar) public onlyRegistrar {
+        require(newRegistrar != address(0), "New registrar cannot be zero address");
+        require(newRegistrar != msg.sender, "Cannot transfer to self");
+
+        // Grant the role to the new registrar
+        _grantRole(REGISTRAR_ROLE, newRegistrar);
+
+        // Revoke the role from the current holder
+        _revokeRole(REGISTRAR_ROLE, msg.sender);
+
+        emit RegistrarRoleTransferred(msg.sender, newRegistrar);
+    }
+
+    // Get contender details by code
+    function getContender(string memory code) public view returns(ContDetails memory) {
+        address cont = codeToAddress[code];
+        require(cont != address(0), "Contender not found");
+        return contenderDetails[cont];
+    }
+
+    // Get contender details by address
+    function getContenderByAddress(address cont) public view returns(ContDetails memory) {
+        require(contenderDetails[cont].exists, "Contender not found");
+        return contenderDetails[cont];
+    }
+
+    // Get all contenders
+    function getAllContenders() public view returns(address[] memory) {
+        return contendersList;
+    }
+
+    // Get vote counts for all contenders
+    function getVoteCounts() public view returns(address[] memory, uint32[] memory) {
+        uint256 length = contendersList.length;
+        address[] memory addresses = new address[](length);
+        uint32[] memory votes = new uint32[](length);
+
+        for (uint256 i = 0; i < length; i++) {
+            addresses[i] = contendersList[i];
+            votes[i] = contenderDetails[contendersList[i]].votersNo;
+        }
+
+        return (addresses, votes);
+    }
+
+    // Get winners (can be called after voting ends)
+    function getWinners() public view returns(address[] memory, uint32) {
+        require(votingEnded, "Voting has not ended");
+
+        uint32 highestVotes = 0;
+        for (uint256 i = 0; i < contendersList.length; i++) {
+            uint32 votes = contenderDetails[contendersList[i]].votersNo;
+            if (votes > highestVotes) {
+                highestVotes = votes;
+            }
+        }
+
+        address[] memory winners = new address[](contendersList.length);
+        uint256 winnerCount = 0;
+        
+        for (uint256 i = 0; i < contendersList.length; i++) {
+            if (contenderDetails[contendersList[i]].votersNo == highestVotes) {
+                winners[winnerCount] = contendersList[i];
+                winnerCount++;
+            }
+        }
+
+        address[] memory finalWinners = new address[](winnerCount);
+        for (uint256 i = 0; i < winnerCount; i++) {
+            finalWinners[i] = winners[i];
+        }
+
+        return (finalWinners, highestVotes);
+    }
+
+    // Check if voting time has expired
+    function hasVotingExpired() public view returns(bool) {
+        return votingActive && block.timestamp > votingEndTime;
+    }
+
+    // Get time remaining for voting
+    function getTimeRemaining() public view returns(uint256) {
+        if (!votingActive || block.timestamp >= votingEndTime) {
+            return 0;
+        }
+        return votingEndTime - block.timestamp;
+    }
+
+    // Overrides required by Solidity
+    function supportsInterface(bytes4 interfaceId) public view override(AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
 }
